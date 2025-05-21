45562f92cf6f8f2b990ea62a8f1da99b6288cef1
Kunal Mathur <kunalmathur0803@gmail.com>
2025-05-21 16:23:29 +0530

    completed config -- 2

--- nvim/.config/nvim/lua/nvim-config/lazy.lua
+++ nvim/.config/nvim/lua/nvim-config/lazy.lua
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup(
     {
         { import = "nvim-config.plugins" },
---        { import = "nvim-config.plugins.lsp" },
+    { import = "nvim-config.plugins.lsp" },
     },
     {
         checker = {
